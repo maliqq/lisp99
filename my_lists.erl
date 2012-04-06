f94561ec64bafce17e5a51fe5c63511c164d5646
Malik Baktiyarov <malik@lookatme.ru>
2012-04-06 20:36:21 +0400

    uniq1

--- my_lists.erl
+++ my_lists.erl
@@ -4,39 +4,52 @@
 -define(MY_LIST, [1, 2, 3, 4, 5, 6, 7]).
 
 %% find the last box of a list.
-lists_last([_|T = [_|_]]) ->
-	lists_last(T);
-lists_last([H|_]) ->
-	H.
+last([_|T=[_|_]]) -> last(T);
+last([H|_]) -> H.
 
 %% find the last but one box of a list.
-lists_last2([_|T=[_,_|_]]) ->
-	lists_last2(T);
-lists_last2(L) ->
-	L.
+last2([_|T=[_,_|_]]) -> last2(T);
+last2(L) -> L.
 
 %% find the nth element of a list
-lists_nth([H|_], N) when N == 1 ->
-    H;
-lists_nth([_|T], N) ->
-    lists_nth(T, N - 1).
+nth([H|_], N) when N == 1 -> H;
+nth([_|T], N) -> nth(T, N - 1).
 
 %% find the number of elements of a list
-lists_size([]) ->
-    0;
-lists_size([_|T]) ->
-    lists_size(T) + 1.
+lsize([]) -> 0;
+lsize([_|T]) -> lsize(T) + 1.
 
 %% reverse a list
-lists_reverse([H|[]]) ->
+lreverse([H|[]]) ->
     [H];
-lists_reverse([H|T]) ->
-    lists_reverse(T) ++ [H].
+lreverse([H|T]) ->
+    lreverse(T) ++ [H].
+
+%% find out whether a list is a palindrome.
+is_palindrome([]) -> true;
+is_palindrome([_|_=[]]) -> true;
+is_palindrome(L) -> L == lreverse(L).
+
+%% flatten a nested list structure
+flatten([]) -> [];
+flatten([H|T]) when is_list(H) -> flatten(H) ++ flatten(T);
+flatten([H|T]) -> [H] ++ flatten(T).
+
+%% eliminate consecutive duplicates of list elements
+scan_while([H|T], H2) when H == H2 -> scan_while(T, H2);
+scan_while(_=[], _) -> [];
+scan_while([H|T], H2) -> uniq1(T).
+
+uniq1([H|_=[H2|T]]) when H == H2 -> [H] ++ scan_while(T, H);
+uniq1([H|T]) -> [H] ++ uniq1(T).
 
 my_lists() ->
-	io:format("last: ~p~n", [lists_last(?MY_LIST)]),
-	io:format("last but one: ~p~n", [lists_last2(?MY_LIST)]),
-	io:format("5th: ~p~n", [lists_nth(?MY_LIST, 5)]),
-	io:format("size: ~p~n", [lists_size(?MY_LIST)]),
-	io:format("reverse: ~p~n", [lists_reverse(?MY_LIST)]).
+	io:format("last: ~p~n", [last(?MY_LIST)]),
+	io:format("last but one: ~p~n", [last2(?MY_LIST)]),
+	io:format("5th: ~p~n", [nth(?MY_LIST, 5)]),
+	io:format("size: ~p~n", [lsize(?MY_LIST)]),
+	io:format("reverse: ~p~n", [lreverse(?MY_LIST)]),
+	io:format("palindrome? ~p~n", [is_palindrome([1, 2, 3, 2, 1, 2])]),
+	io:format("flatten ~p~n", [flatten([1, [2, 3], [4, 5, 6], [7], 8, 9])]),
+	io:format("uniq1 ~p~n", [uniq1([1, 1, 1, 1, 1, 2, 3, 1, 1, 2, 2, 2, 4, 5, 6, 7, 7])]).
 
